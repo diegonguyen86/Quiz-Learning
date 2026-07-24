c84a4157fea9cbc651d3e6ab8d6d68d3598f0d6b
diegonguyen86 <146827007+diegonguyen86@users.noreply.github.com>
2026-07-25 01:54:33 +0700

    g

--- check_quiz_list.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('index.html', 'utf-8');
-console.log('Has quiz-list:', html.includes('id="quiz-list"'));
--- view_dash.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('index.html', 'utf-8');
-const lines = html.split(/\r?\n/);
-const idx = lines.findIndex(l => l.includes('id="dashboard-screen"'));
-if (idx !== -1) {
-    console.log(lines.slice(idx, idx + 30).join('\n'));
-}
